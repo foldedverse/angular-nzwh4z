b78bfaf3ba262b6d20a1da8f7f4c8feaf3d64537
foldedverse <121286569+foldedverse@users.noreply.github.com>
2023-06-24 17:02:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nzwh4z
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nzwh4z)
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ChatWidgetComponent } from './chat-widget/chat-widget.component'; // Import the ChatWidgetComponent
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ChatWidgetComponent // Declare the ChatWidgetComponent here
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
